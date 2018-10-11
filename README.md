35b34c23bc82e476018ea86f32233023fc0b9168
carvo <ccarvo@gmail.com>
2018-10-11 20:42:36 -0300

    [add] - readme and swagger

---
+++ README.md
@@ -0,0 +1,22 @@
+# DNA Analysis API
+
+## Assumptions
+* DNA is a square matrix of chars
+
+## Requirements
+* Java 8
+* Maven 3.x (properly configured)
+* Port **8088** free (if already in use
+        edit the [application.properties](src/main/resources/application.properties))
+
+## To run
+
+### Local
+In PROJECT_ROOT path ```mvn spring-boot:run```
+
+## API doc
+
+### Local
+Access
+[http://localhost:8088/swagger-ui.html](http://localhost:8088/swagger-ui.html)
+after running.
--- src/main/java/br/com/mercadolivre/mutantidentifier/controllers/DnaAnalysisController.java
+++ src/main/java/br/com/mercadolivre/mutantidentifier/controllers/DnaAnalysisController.java
@@ -3,6 +3,9 @@ package br.com.mercadolivre.mutantidentifier.controllers;
 import br.com.mercadolivre.mutantidentifier.UrlMapping;
 import br.com.mercadolivre.mutantidentifier.analysis.DnaService;
 import br.com.mercadolivre.mutantidentifier.analysis.validators.DnaStructureValidator;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,14 +24,27 @@ public class DnaAnalysisController {
     @Autowired
     private DnaStructureValidator structureValidator;
 
+    @ApiOperation(value = "${DnaAnalysisController.postDna.desc}", notes = "${DnaAnalysisController.postDna.notes}")
+    @ApiResponses({
+            @ApiResponse(code = 200, response = String.class, message = "OK" +
+                    "<br>Is a mutant"
+            ),
+            @ApiResponse(code = 400, response = String.class, message = "Bad Request" +
+                    "<br>Possible causes: " +
+                    "<pre>  &bull; " + DnaStructureValidator.MSG_NULL_OR_EMPTY +
+                    "<br>  &bull; " + DnaStructureValidator.MSG_NOT_SQUARE + "</pre>"
+            ),
+            @ApiResponse(code = 403, response = String.class, message = "Forbidden" +
+                    "<br>Is not a mutant"
+            ),
+    })
     @PostMapping(UrlMapping.POST_DNA)
-    public ResponseEntity<String> handleMutant(@RequestBody DnaAnalysisRequest request) {
+    public ResponseEntity<String> postDna(@RequestBody DnaAnalysisRequest request) {
         final Optional<String> error = structureValidator.validate(request.getDna());
 
         if (error.isPresent()) {
             return new ResponseEntity<>(error.get(), HttpStatus.BAD_REQUEST);
-        }
-        else {
+        } else {
             return invokeService(request);
         }
     }
