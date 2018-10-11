a5f7a090750758a091c61976cf1ab2e6e104426b
carvo <ccarvo@gmail.com>
2018-10-11 18:35:22 -0300

    [add] - integration test starting in controller

---
+++ src/integrated-test/java/br/com/mercadolivre/mutantidentifier/integratedtest/analysis/DnaServiceIntegratedTest.java
@@ -0,0 +1,144 @@
+package br.com.mercadolivre.mutantidentifier.integratedtest.analysis;
+
+import br.com.mercadolivre.mutantidentifier.analysis.DnaService;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DnaServiceIntegratedTest {
+
+    @Test
+    public void isMutantOnlyInVertical() {
+        final String[] dna = {
+                "CACATAAA",
+                "CTCGGGAT",
+                "CACATAAC",
+                "CAAATGAG",
+                "ATTTGAAA",
+                "AAGTTCAT",
+                "TTGTGTTC",
+                "AGAAGGCG"
+        };
+
+        final boolean b = new DnaService().isMutant(dna);
+
+        assertTrue(b);
+    }
+
+    @Test
+    public void isMutantOnlyInHorizontal() {
+        final String[] dna = {
+                "AAAATCGA",
+                "CAGTGCTA",
+                "TTATGTCT",
+                "AGAAGGAT",
+                "CTCCTAAT",
+                "AAGGTTCC",
+                "AAGGCTCC",
+                "GGAATTTT"
+        };
+
+        final boolean b = new DnaService().isMutant(dna);
+
+        assertTrue(b);
+    }
+
+    @Test
+    public void isMutantOnlyInHorizontalSameLine() {
+        final String[] dna = {
+                "ACAATCGA",
+                "CAGTGCTA",
+                "TTATGTCT",
+                "AGAAGGAT",
+                "CTCCTAAT",
+                "AAGGTTCC",
+                "AAGGCTCC",
+                "TTTTTTTT"
+        };
+
+        final boolean b = new DnaService().isMutant(dna);
+
+        assertTrue(b);
+    }
+
+    /**
+     * Horizontal: AAAAAAAA first line
+     * Oblique: TTTT last four lines
+     */
+    @Test
+    public void isMutantInHorizontalPlusOblique() {
+        final String[] dna = {
+                "AAAATCTA",
+                "CCGTGCTA",
+                "TTCTGTCT",
+                "AGAAGGAT",
+                "CTCCTAAT",
+                "AAGGTTCC",
+                "AAGGCTTC",
+                "GGAATGTT"
+        };
+
+        final boolean b = new DnaService().isMutant(dna);
+
+        assertTrue(b);
+    }
+
+    /**
+     * Vertical: CCCC first column last lines
+     * Oblique: TTTT last four lines
+     */
+    @Test
+    public void isMutantInVerticalPlusOblique() {
+        final String[] dna = {
+                "ACGTATAA",
+                "CCGTGCTA",
+                "TTATGTCT",
+                "AGAAGGAT",
+                "CTCCTAAT",
+                "CAGGTTCC",
+                "CAGGCTTC",
+                "CGAATCTT"
+        };
+
+        final boolean b = new DnaService().isMutant(dna);
+
+        assertTrue(b);
+    }
+
+    @Test
+    public void isMutantOnlySouthwestOblique() {
+        final String[] dna = {
+                "ACGTATAA",
+                "CCGTTCTA",
+                "TTATGTCT",
+                "AGTAGGAT",
+                "CTCCTAAT",
+                "CAGGTTTC",
+                "TAGGCTTC",
+                "CGAATCTC"
+        };
+
+        final boolean b = new DnaService().isMutant(dna);
+
+        assertTrue(b);
+    }
+
+    @Test
+    public void isMutantOnlySoutheastOblique() {
+        final String[] dna = {
+                "ACGTATAA",
+                "CCGTCCTA",
+                "TTATGTCT",
+                "AGTAGGAT",
+                "CTCATAAT",
+                "CAAGTTTC",
+                "TAGGCTTC",
+                "AGAATCTT"
+        };
+
+        final boolean b = new DnaService().isMutant(dna);
+
+        assertTrue(b);
+    }
+
+}
