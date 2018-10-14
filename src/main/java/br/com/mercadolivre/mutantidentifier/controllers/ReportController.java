ab7570e862f986f337f8085b67a651ab391f7fc0
carvo <ccarvo@gmail.com>
2018-10-14 18:18:34 -0300

    [up] api/swagger docs

--- src/main/java/br/com/mercadolivre/mutantidentifier/controllers/ReportController.java
+++ src/main/java/br/com/mercadolivre/mutantidentifier/controllers/ReportController.java
@@ -3,6 +3,7 @@ package br.com.mercadolivre.mutantidentifier.controllers;
 import br.com.mercadolivre.mutantidentifier.UrlMapping;
 import br.com.mercadolivre.mutantidentifier.model.Stats;
 import br.com.mercadolivre.mutantidentifier.reports.ReportService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,7 @@ public class ReportController {
     @Autowired
     private ReportService reportService;
 
+    @ApiOperation(value = "${ReportController.getStats.desc}", notes = "${ReportController.getStats.notes}")
     @GetMapping(UrlMapping.STATS)
     public ResponseEntity<Stats> getStats() {
         final Stats stats = reportService.getStats();
